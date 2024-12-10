df04a424d97ad781ae3e1ad522d15a3234babd46
Samiran2004 <samirans170@gmail.com>
2024-12-10 23:08:12 +0530

    add/22, test.csv

---
+++ 22_while_with_csv.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+while IFS="," read  id name age
+do
+	echo "Id: $id"
+	echo "Name: $name"
+	echo "Age: $age"
+done < test.csv
+
+#Another process...
+
+echo "Using Another Process"
+cat test.csv | awk 'NR!=1 {print}' | while IFS="," read id name age
+do
+	echo "Id is: $id"
+done
