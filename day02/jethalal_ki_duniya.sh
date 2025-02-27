b744bc91292a180366a0d18de68f922ed89be84d
Samiran2004 <samirans170@gmail.com>
2025-02-27 07:46:44 +0530

    add/day02

---
+++ day02/jethalal_ki_duniya.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# This is Jethalal ki duniya
+
+<< comment
+ Anything 
+written 
+here will not be executed
+comment
+
+name="BabitaJii"
+
+echo "Name is $name, and date is $(date)"
+
+echo "Enter your name: "
+read username
+
+echo "Your name is: $username"
+
+echo "The characters in $0 $1"
