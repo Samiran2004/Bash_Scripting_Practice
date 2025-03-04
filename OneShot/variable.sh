2212de75aa34344ce8b99cddc38abe98060cb07d
Samiran2004 <samirans170@gmail.com>
2025-03-04 23:16:42 +0530

    add/variable.sh

---
+++ OneShot/variable.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+<< comment
+This is multiline comment...
+comment
+
+name="Samiran Samanta"
+echo "Name: $name"
+echo "Date is: $(date)"
+
+read -p "Enter User name: " username
+echo "You Entered: $username"
