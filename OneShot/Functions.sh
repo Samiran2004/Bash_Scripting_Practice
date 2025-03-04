62a326985ee44688c4ed9bde716b6bb45e5d7f65
Samiran2004 <samirans170@gmail.com>
2025-03-05 00:22:49 +0530

    add/Check Even Odd using function

---
+++ OneShot/Functions.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Check the number is even or not...
+#This is function defination...
+function isEven() {
+	if [[ $(($1%2)) -eq 0 ]]
+	then
+		echo "$1 is an even number"
+	else
+		echo "$1 is not an even number..."
+	fi
+}
+
+read -p "Enter a number to check the number is even or not: " num
+isEven $num
