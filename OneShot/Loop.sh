0b3d00c3f3e13ad6eb61c21e92fb21bd4fc94908
Samiran2004 <samirans170@gmail.com>
2025-03-04 23:52:36 +0530

    add/Loop.sh

--- OneShot/Loop.sh
+++ OneShot/Loop.sh
@@ -4,4 +4,16 @@
 This is the script to learn loop...
 comment
 
+read -p "Enter the folder name: " foldername
+read -p "How much folder you want to create: " range
+for (( i=1 ; i<=$range ; i++ ))
+do
+	mkdir "$foldername$i"
+	echo "$foldername$i created..."
+done
 
+for ((i=1 ; i<=$range ; i++))
+do
+	rmdir "$foldername$i"
+	echo "$foldername$i deleted..."
+done
